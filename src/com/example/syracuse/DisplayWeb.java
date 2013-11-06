e482041bc6f4ca98580c69390696547faeb18aa6
Carl Poole <carl@carlpoole.com>
2013-11-06 18:59:40 -0500

    Connected app to website so it pulls the speech from a text file instead of it being hardcoded

--- src/com/example/syracuse/DisplayWeb.java
+++ src/com/example/syracuse/DisplayWeb.java
@@ -50,6 +50,7 @@ public class DisplayWeb extends Activity{
 	
 	TextView mytextview;
 	private WakeLock mWakeLock;
+	public String SpeechString;
 
 	@Override
 	public boolean onGenericMotionEvent(MotionEvent event)
@@ -64,23 +65,46 @@ public class DisplayWeb extends Activity{
 		
 		gestureDetector = new GestureDetector(this, new MyGestureListener());
 		
-		
 		setContentView(R.layout.activity_main);
 		
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 						
 		mytextview = (TextView) findViewById(R.id.textView1);
+		
+		
+		ThreadPolicy tp = ThreadPolicy.LAX;
+		StrictMode.setThreadPolicy(tp);
+		
+		HttpClient httpclient = new DefaultHttpClient();
+		HttpGet httppost = new HttpGet("http://carlpoole.com/glass_scroll.html");
 
-		mytextview.setText("Four score and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal. /n Now we are engaged in a great civil war, testing whether that nation, or any nation so conceived and so dedicated, can long endure. We are met on a great battle-field of that war. We have come to dedicate a portion of that field, as a final resting place for those who here gave their lives that that nation might live. It is altogether fitting and proper that we should do this./n But, in a larger sense, we can not dedicate -- we can not consecrate -- we can not hallow -- this ground. The brave men, living and dead, who struggled here, have consecrated it, far above our poor power to add or detract. The world will little note, nor long remember what we say here, but it can never forget what they did here. It is for us the living, rather, to be dedicated here to the unfinished work which they who fought here have thus far so nobly advanced. It is rather for us to be here dedicated to the great task remaining before us -- that from these honored dead we take increased devotion to that cause for which they gave the last full measure of devotion -- that we here highly resolve that these dead shall not have died in vain -- that this nation, under God, shall have a new birth of freedom -- and that government of the people, by the people, for the people, shall not perish from the earth."
-				);
-	
+		InputStream inputStream = null;
+		
+		try {
+
+		HttpResponse response = httpclient.execute(httppost);           
+		HttpEntity entity = response.getEntity();
+
+		SpeechString = EntityUtils.toString(entity);
+
+		Log.w("HTTPRESPONSE", SpeechString);
+
+		} catch (Exception e) { 
+
+		} finally {
+
+		try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+
+		}
+
+		mytextview.setText(SpeechString);
 	}
+	
 	public class MyGestureListener extends android.view.GestureDetector.SimpleOnGestureListener
 	{
 	    @Override
 	    public boolean onFling(MotionEvent start, MotionEvent finish, float velocityX, float velocityY)
 	    {     
-	    	
 	    	if (velocityX>0) {
 	    		ScrollView sv = (ScrollView)findViewById(R.id.scrollView1);
 	    		sv.smoothScrollBy(0, 150);
@@ -93,6 +117,5 @@ public class DisplayWeb extends Activity{
 	        return true;
 	    } 
 	}
-
 }
 
