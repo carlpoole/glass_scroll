ac8396c5b43a949938d3fae257760cf1150e8774
Carl Poole <carl@carlpoole.com>
2013-12-07 20:46:40 -0500

    Added "Ok Glass" command

--- gen/com/example/syracuse/R.java
+++ gen/com/example/syracuse/R.java
@@ -17,30 +17,32 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090002;
-        public static final int scrollView1=0x7f090000;
-        public static final int textView1=0x7f090001;
+        public static final int action_settings=0x7f0a0002;
+        public static final int scrollView1=0x7f0a0000;
+        public static final int textView1=0x7f0a0001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
+        public static final int main=0x7f090000;
     }
     public static final class raw {
-        public static final int sms=0x7f040000;
+        public static final int sms=0x7f050000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060002;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int glass_voice_trigger=0x7f070003;
+        public static final int hello_world=0x7f070002;
+        public static final int stop=0x7f070004;
     }
     public static final class style {
         /** 
@@ -64,10 +66,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
+    }
+    public static final class xml {
+        public static final int voice_trigger_start=0x7f040000;
     }
 }
